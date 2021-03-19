844be223edd23d0b7761f2b532481e8d205ebb2f
Giovanni Rodriguez <ggabriel92@hotmail.com>
2021-03-19 06:45:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,12 +22,17 @@ function add2(n) {
   return n + 2;
 
   // Feel free to move things around!
-
+  
 }
 
 
-var theFunk = funkyFunction ()
-theFunk = "FUNKY!";
+var funkyFunction = function()  {
+  return function() {
+    return "FUNKY!";
+  }
+}
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
+var theFunk = funkyFunction ()
+theFunk = "FUNKY!";
